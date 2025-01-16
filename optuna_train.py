fefd6392564226b59f4b84f0c4ab07fbd9e476b3
izzak98 <izzak98@hotmail.com>
2025-01-16 20:45:11 +0000

    Load database path from config.json in optuna_train.py

--- optuna_train.py
+++ optuna_train.py
@@ -1,7 +1,11 @@
+import json
 import optuna
 
 from LSTM import objective as lstm_objective
 from Dense import objective as dense_objective
+
+with open('config.json', encoding="utf8") as f:
+    CONFIG = json.load(f)
 
 
 def train_lstm():
@@ -10,15 +14,16 @@ def train_lstm():
     study = optuna.create_study(
         direction="minimize",
         study_name="LSTM",
-        storage="sqlite:///LSTM.db",
+        storage=CONFIG["general"]["db_path"],
         load_if_exists=True,
         sampler=sampler,
-        )
+    )
     n_trials = n_trials - len(study.trials)
     study.optimize(lstm_objective, n_trials=n_trials, n_jobs=1)
     print(study.best_params)
     print(study.best_value)
     print(study.best_trial)
+
 
 def train_dense():
     n_trials = 100
@@ -26,16 +31,17 @@ def train_dense():
     study = optuna.create_study(
         direction="minimize",
         study_name="Dense",
-        storage="sqlite:///Dense.db",
+        storage=CONFIG["general"]["db_path"],
         load_if_exists=True,
         sampler=sampler,
-        )
+    )
     finished_trials = [t for t in study.trials if t.state == optuna.trial.TrialState.COMPLETE]
     n_trials = n_trials - len(finished_trials)
     study.optimize(dense_objective, n_trials=n_trials, n_jobs=1)
     print(study.best_params)
     print(study.best_value)
     print(study.best_trial)
+
 
 if __name__ == "__main__":
     train_lstm()
